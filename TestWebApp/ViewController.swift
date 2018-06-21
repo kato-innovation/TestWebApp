b2b8558b7db04700720889e659cb664f5ba6354a
Sugiyama Toyokazu <tsugiyama@sugiyamanomacbook-puro.local>
2018-06-21 19:16:32 +0900

    Initial Commit

---
+++ TestWebApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestWebApp
+//
+//  Created by Sugiyama Toyokazu on 2018/06/21.
+//  Copyright © 2018年 SEKISUI KINZOKU CO., LTD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
